6cfaf7744d1482c70549858cc6245c0c009881c7
nazmul-prince <nazmul.prince@groupbuilder.fi>
2019-07-22 19:29:07 +0600

    Added alert

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Alert from './components/layout/Alert';
+import Users from './components/users/Users';
+import Search from './components/users/Search';
+import axios from 'axios';
 import './App.css';
+import { async } from 'q';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    users: [],
+    loading: false,
+    alert: null
+  };
+
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+
+  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+  //   client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  //Search github users
+  searchUsers = async text => {
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
+    client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ users: res.data.items, loading: false });
+  }
+
+  //Clear github users
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg: msg, type: type } });
+  }
+
+  render() {
+    console.log("Renderring app.js");
+    const { users, loading, alert } = this.state;
+
+    return (
+      <div className="App">
+        <Navbar title="Github Finder" icon="fab fa-github"/>
+        <div className="container">
+          <Alert alert={alert} />
+          <Search 
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+           />
+          <Users loading={loading} users={users}/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
