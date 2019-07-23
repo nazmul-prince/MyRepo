d1027283b12de95099f0515815d4f3c784c88361
nazmul-prince <nazmul.prince@groupbuilder.fi>
2019-07-23 18:39:49 +0600

    Added about page t-21

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About this app</h1>
+            <p>App to search github users</p>
+            <p>Version 1.1.0</p>
+        </div>
+    )
+}
+
+export default About;
